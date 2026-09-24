9890f984d64baca112c309b5ca3b8295fead69be
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 13:26:25 +0700

    Create 03-orang.js

---
+++ js/data/grafis/htp/03-orang.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_HTP_SLIDES = window.GRAFIS_AUTO_DATA_HTP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_HTP_SLIDES.push({
+  id: 'htp-03', title: '3. Orang',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
