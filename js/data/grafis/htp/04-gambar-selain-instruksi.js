3bf67a8bd4ecd0540be4d59be62152f933739f5b
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 13:26:42 +0700

    Create 04-gambar-selain-instruksi.js

---
+++ js/data/grafis/htp/04-gambar-selain-instruksi.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_HTP_SLIDES = window.GRAFIS_AUTO_DATA_HTP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_HTP_SLIDES.push({
+  id: 'htp-04', title: '4. Gambar Selain dari Instruksi',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
