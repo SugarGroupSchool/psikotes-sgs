35f75082b5aa977e40af1e769c2ed5edd0c201ca
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 10:06:16 +0700

    Create 01-ukuran-gambar.js

---
+++ js/grafis/dap/01-ukuran-gambar.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
+  id: 'dap-01', title: '1. Ukuran Gambar',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
