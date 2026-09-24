ad7bee29934d251a859b7b08b5b0c08176bf01be
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 13:25:58 +0700

    Create 01-rumah.js

---
+++ js/data/grafis/htp/01-rumah.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_HTP_SLIDES = window.GRAFIS_AUTO_DATA_HTP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_HTP_SLIDES.push({
+  id: 'htp-01', title: '1. Rumah',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
