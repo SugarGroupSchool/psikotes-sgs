a8450be65195f79c29fcbffede6ac8acca91f381
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 10:07:41 +0700

    Create 05-bagian-kepala.js

---
+++ js/data/grafis/dap/05-bagian-kepala.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
+  id: 'dap-05', title: '5. Bagian Kepala',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
