9221caec1cb87a2de0fdf62565d0da3ac61db8bc
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 10:06:30 +0700

    Create 02-kesan-umum.js

---
+++ js/grafis/dap/02-kesan-umum.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
+  id: 'dap-02', title: '2. Kesan Umum',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
