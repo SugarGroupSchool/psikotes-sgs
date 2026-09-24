4e98ba3bf251dbb63df7e93abb755f2e12dbcdea
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 11:56:32 +0700

    Delete js/grafis directory

--- js/grafis/dap/04-simbolisme-garis.js
+++
@@ -1,6 +0,0 @@
-window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
-window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
-  id: 'dap-04', title: '4. Simbolisme Garis',
-  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
-  sections: []
-});
