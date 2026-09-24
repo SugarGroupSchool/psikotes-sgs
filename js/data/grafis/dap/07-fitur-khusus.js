9a5a3c898b4c4b2702b1afd36f2e4eb682d2b7d2
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 10:08:15 +0700

    Create 07-fitur-khusus.js

---
+++ js/data/grafis/dap/07-fitur-khusus.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
+  id: 'dap-07', title: '7. Fitur Khusus',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
