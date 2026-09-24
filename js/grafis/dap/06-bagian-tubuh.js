8e26f745672cea7415082b9f612fcd6eb35216ea
SugarGroupSchool <hca2.sgschools@gmail.com>
2026-09-24 10:08:01 +0700

    Create 06-bagian-tubuh.js

---
+++ js/grafis/dap/06-bagian-tubuh.js
@@ -0,0 +1,6 @@
+window.GRAFIS_AUTO_DATA_DAP_SLIDES = window.GRAFIS_AUTO_DATA_DAP_SLIDES || [];
+window.GRAFIS_AUTO_DATA_DAP_SLIDES.push({
+  id: 'dap-06', title: '6. Bagian Tubuh',
+  image: 'https://raw.githubusercontent.com/Pragas123/assets/refs/heads/main/nmqo6a.png',
+  sections: []
+});
